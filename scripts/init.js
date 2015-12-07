4816ecb0888611a945f75c8c0ef0219e85b536ac
Mark Monteiro <mark.monteiro23@gmail.com>
2015-12-06 23:04:25 -0500

    add responsive to spell table

--- scripts/init.js
+++ scripts/init.js
@@ -16,7 +16,6 @@ $(function () {
       var dataTable = $(this);
 
       dataTable = dataTable.DataTable({
-         paging: false,
          // dom: "<'row'<'col-sm-6'l><'col-sm-6'f>>" +
          //      "<'row'<'col-sm-12'tr>>" +
          //      "<'row'<'col-sm-5'i><'col-sm-7'p>>",
