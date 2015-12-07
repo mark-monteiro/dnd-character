61be73722e7c4d16df6f8a6cd4c99edca181b361
Mark Monteiro <mark.monteiro23@gmail.com>
2015-12-06 19:16:27 -0500

    add spells page; include jquery datatables and bootstrap typeahead

---
+++ scripts/init.js
@@ -0,0 +1,71 @@
+$(function () {
+   // Init Tooltips
+   $('[data-toggle="tooltip"]').each(function() {
+      var elem = $(this);
+
+      if (elem.hasClass('overflow-ellipses') && this.offsetHeight < this.scrollHeight || this.offsetWidth < this.scrollWidth)
+         return;
+
+      elem.tooltip({
+         'trigger': 'hover'
+      });
+   });
+
+   // Init Datatables
+   $('.datatable').each(function() {
+      var dataTable = $(this);
+
+      dataTable = dataTable.DataTable({
+         paging: false,
+         // dom: "<'row'<'col-sm-6'l><'col-sm-6'f>>" +
+         //      "<'row'<'col-sm-12'tr>>" +
+         //      "<'row'<'col-sm-5'i><'col-sm-7'p>>",
+      });
+   });
+
+   // Init datatable search forms
+   $('.search-form').each(function() {
+      var fieldset = $(this);
+      var searchInputs = fieldset.find(":input");
+      var targetTable = $(fieldset.data('target'));
+
+      targetTable.closest('.dataTables_wrapper').find('.dataTables_filter').remove();
+
+      searchInputs.change(function(event) {
+         targetTable.DataTable().draw();
+      });
+
+      searchInputs.filter("[type='text']").keyup(function() {
+         targetTable.DataTable().draw();
+      });
+
+      $.fn.dataTable.ext.search.push(
+         function( settings, data, dataIndex ) {
+            var success = true;
+
+            searchInputs.each(function() {
+               var inputValid;
+               var input = $(this);
+               if(input.val() == "") return;
+
+               var target = input.data('target-col');
+               var filterData = input.val().toLowerCase();
+               var targetData = data[target].toLowerCase();
+
+               if(input.is("[type='text']"))
+               {
+                  inputValid = targetData.indexOf(filterData) > -1;
+               }
+               else
+               {
+                  inputValid = filterData == targetData;
+               }
+
+                return success = success && inputValid;
+            });
+
+            return success;
+         }
+      );
+   });
+});
